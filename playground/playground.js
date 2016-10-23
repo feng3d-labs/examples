6f89e8450e2dbfb2b4db36b0c7a6f5aedac294b8
feng <908087098@qq.com>
2016-10-23 14:47:28 +0800

    调整 feng3d.js 相关配置

--- playground/playground.js
+++ playground/playground.js
@@ -6,7 +6,7 @@
 
 	window.onload = function () {
 		require(['vs/editor/editor.main'], function () {
-			xhr('libs/feng3d.d.ts').then(function (response) {
+			xhr('feng3d/release/feng3d.d.ts').then(function (response) {
 				monaco.languages.typescript.javascriptDefaults.addExtraLib(response.responseText, 'monaco.d.ts');
 				monaco.languages.typescript.javascriptDefaults.addExtraLib([
 					'declare var require: {',

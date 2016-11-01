a5c7d0a77862326c9d5e80021d21c0cdce751114
feng <908087098@qq.com>
2016-11-01 23:30:58 +0800

    调整 示例

--- src/examples/all.ts
+++ src/examples/all.ts
@@ -3,24 +3,24 @@ var PLAY_SAMPLES = [
 		chapter: "Basic",
 		name: "container3d",
 		id: "basic-container3d",
-		path: "basic/container3d"
+		path: "basic/container3dTest"
 	},
 	{
 		chapter: "Basic",
 		name: "primitives",
 		id: "basic-primitives",
-		path: "basic/primitives"
+		path: "basic/primitivesTest"
 	},
 	{
 		chapter: "Basic",
 		name: "segment",
 		id: "basic-segment",
-		path: "basic/segment"
+		path: "basic/segmentTest"
 	},
 	{
 		chapter: "Material",
 		name: "colorMaterial",
 		id: "material-colorMaterial",
-		path: "material/colorMaterial"
+		path: "material/colorMaterialTest"
 	}
 ];

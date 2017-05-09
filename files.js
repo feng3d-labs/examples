f553b003cd8a337fbba90444c767f59137e89ac2
feng <908087098@qq.com>
2017-05-09 19:56:12 +0800

    引入 three.js 的examples，index.html

---
+++ files.js
@@ -0,0 +1,34 @@
+var files = {
+    "": [
+        "Container3DTest",
+        "FPSControllerTest",
+        "BillboardTest",
+        "MousePickTest",
+        "SkyBoxTest",
+    ],
+    "material": [
+        "ColorMaterialTest",
+        "PointMaterialTest",
+        "SegmentMaterialTest",
+        "StandardMaterialTest",
+        "TextureMaterialTest",
+    ],
+    "geometry": [
+        "PrimitiveTest",
+        "GeometryTest",
+    ],
+    "lights": [
+        "PointLightTest",
+    ],
+    "animator": [
+        "ParticleAnimatorTest",
+        "SceneLoadTest",
+    ],
+    "loader": [
+        "OBJParserTest",
+        "MD5LoaderTest",
+    ],
+    "advanced": [
+        "TerrainTest",
+    ]
+};

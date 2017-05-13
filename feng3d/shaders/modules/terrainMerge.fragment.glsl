52cfcbf65f9dd7e854c35acd3920515847481cba
feng <908087098@qq.com>
2017-05-13 14:53:11 +0800

    调整 地形合并shader

--- feng3d/shaders/modules/terrainMerge.fragment.glsl
+++ feng3d/shaders/modules/terrainMerge.fragment.glsl
@@ -12,7 +12,9 @@ vec4 offset[3];
 vec2 tileSize = vec2(512.0,512.0);
 float maxLod = 7.0;
 
-vec4 terrainTexture2DLod(sampler2D s_splatMergeTexture,vec2 t_uv,float lod){
+vec4 terrainTexture2DLod(sampler2D s_splatMergeTexture,vec2 t_uv,float lod,vec4 offset){
+
+        // t_uv = t_uv * offset[i].xy + offset[i].zw;
 
     //计算不同lod像素缩放以及起始坐标
     vec4 lodvec = vec4(0.5,1.0,0.0,0.0);
@@ -22,12 +24,11 @@ vec4 terrainTexture2DLod(sampler2D s_splatMergeTexture,vec2 t_uv,float lod){
 
     //lod块尺寸
     vec2 lodSize = lodvec.xy * imageSize;
+    vec4 lodOffset = lodSize.xyxy * offset;
     //lod块中像素索引
     t_uv = t_uv * lodSize;
     //取整像素
-    // t_uv = floor(t_uv) / imageSize;
-    t_uv = (t_uv + 2.0) / imageSize;
-    // t_uv = mod(ceil(t_uv),lodSize) / imageSize;
+    t_uv = (mod(floor(t_uv + vec2(1.0,0.0)),lodOffset.xy) + lodOffset.zw) / imageSize;
     //添加lod起始坐标
     t_uv = t_uv + lodvec.zw;
     
@@ -45,12 +46,12 @@ float mipmapLevel(vec2 uv, vec2 textureSize)
     return 0.5 * log2(d);
 }
 
-vec4 terrainTexture2D(sampler2D s_splatMergeTexture,vec2 t_uv,float lod){
+vec4 terrainTexture2D(sampler2D s_splatMergeTexture,vec2 t_uv,float lod,vec4 offset){
  
     #ifdef LOD_LINEAR
-        vec4 tColor = mix(terrainTexture2DLod(s_splatMergeTexture,t_uv,floor(lod)),terrainTexture2DLod(s_splatMergeTexture,t_uv,ceil(lod)),fract(lod));
+        vec4 tColor = mix(terrainTexture2DLod(s_splatMergeTexture,t_uv,floor(lod),offset),terrainTexture2DLod(s_splatMergeTexture,t_uv,ceil(lod),offset),fract(lod));
     #else
-        vec4 tColor = terrainTexture2DLod(s_splatMergeTexture,t_uv,ceil(lod));
+        vec4 tColor = terrainTexture2DLod(s_splatMergeTexture,t_uv,ceil(lod),offset);
     #endif
 
     return tColor;
@@ -69,8 +70,7 @@ vec4 terrainMethod(vec4 diffuseColor,vec2 v_uv) {
         float lod = mipmapLevel(t_uv,tileSize);
         lod = clamp(lod,0.0,maxLod);
         t_uv = fract(t_uv);
-        t_uv = t_uv * offset[i].xy + offset[i].zw;
-        vec4 tColor = terrainTexture2D(s_splatMergeTexture,t_uv,lod);
+        vec4 tColor = terrainTexture2D(s_splatMergeTexture,t_uv,lod,offset[i]);
         diffuseColor = (tColor - diffuseColor) * blend[i] + diffuseColor;
     }
 
--- src/away3d/Basic_Fire.ts
+++ src/away3d/Basic_Fire.ts
@@ -1,211 +1,211 @@
 module feng3d
 {
-    export class Basic_Fire
-    {
-        public static NUM_FIRES: number = 10;
-        private scene: Scene3D;
-        private camera: GameObject;
-        private view: View3D;
-        private cameraController: HoverController;
-        private planeMaterial: StandardMaterial;
-        private particleMaterial: StandardMaterial;
-        private directionalLight: DirectionalLight;
-        private fireAnimationSet: ParticleAnimator;
-        private particleGeometry: Geometry;
-        private timer: Timer;
-        private plane: GameObject;
-        private fireObjects: Array<FireVO> = new Array<FireVO>();
-        private move: boolean = false;
-        private lastPanAngle: number = NaN;
-        private lastTiltAngle: number = NaN;
-        private lastMouseX: number = NaN;
-        private lastMouseY: number = NaN;
+    // export class Basic_Fire
+    // {
+    //     public static NUM_FIRES: number = 10;
+    //     private scene: Scene3D;
+    //     private camera: GameObject;
+    //     private view: View3D;
+    //     private cameraController: HoverController;
+    //     private planeMaterial: StandardMaterial;
+    //     private particleMaterial: StandardMaterial;
+    //     private directionalLight: DirectionalLight;
+    //     private fireAnimationSet: ParticleAnimator;
+    //     private particleGeometry: Geometry;
+    //     private timer: Timer;
+    //     private plane: GameObject;
+    //     private fireObjects: Array<FireVO> = new Array<FireVO>();
+    //     private move: boolean = false;
+    //     private lastPanAngle: number = NaN;
+    //     private lastTiltAngle: number = NaN;
+    //     private lastMouseX: number = NaN;
+    //     private lastMouseY: number = NaN;
 
-        public constructor()
-        {
-            this.init();
-        }
+    //     public constructor()
+    //     {
+    //         this.init();
+    //     }
 
-        private init()
-        {
-            this.initEngine();
-            this.initLights();
-            this.initMaterials();
-            this.initParticles();
-            this.initObjects();
-            this.initListeners();
-        }
+    //     private init()
+    //     {
+    //         this.initEngine();
+    //         this.initLights();
+    //         this.initMaterials();
+    //         this.initParticles();
+    //         this.initObjects();
+    //         this.initListeners();
+    //     }
 
-        private initEngine()
-        {
-            var canvas = document.getElementById("glcanvas");
-            var view3D = this.view = new View3D(canvas);
+    //     private initEngine()
+    //     {
+    //         var canvas = document.getElementById("glcanvas");
+    //         var view3D = this.view = new View3D(canvas);
 
-            this.camera = view3D.camera;
-            this.scene = view3D.scene;
-            this.cameraController = new HoverController(this.camera);
-            this.cameraController.distance = 1000;
-            this.cameraController.minTiltAngle = 0;
-            this.cameraController.maxTiltAngle = 90;
-            this.cameraController.panAngle = 45;
-            this.cameraController.tiltAngle = 20;
-        }
+    //         this.camera = view3D.camera;
+    //         this.scene = view3D.scene;
+    //         this.cameraController = new HoverController(this.camera);
+    //         this.cameraController.distance = 1000;
+    //         this.cameraController.minTiltAngle = 0;
+    //         this.cameraController.maxTiltAngle = 90;
+    //         this.cameraController.panAngle = 45;
+    //         this.cameraController.tiltAngle = 20;
+    //     }
 
-        private initLights()
-        {
-            this.directionalLight = new DirectionalLight(0, -1, 0);
-            this.directionalLight.castsShadows = false;
-            this.directionalLight.color.fromUnit(0xeedddd);
-            this.directionalLight.intensity = .5;
-            var gameObject = new GameObject();
-            gameObject.addComponent(this.directionalLight);
-            this.scene.addChild(gameObject);
-        }
+    //     private initLights()
+    //     {
+    //         this.directionalLight = new DirectionalLight(0, -1, 0);
+    //         this.directionalLight.castsShadows = false;
+    //         this.directionalLight.color.fromUnit(0xeedddd);
+    //         this.directionalLight.intensity = .5;
+    //         var gameObject = new GameObject();
+    //         gameObject.addComponent(this.directionalLight);
+    //         this.scene.addChild(gameObject);
+    //     }
 
-        private initMaterials()
-        {
-            this.planeMaterial = new StandardMaterial("resources/floor_diffuse.jpg", "resources/floor_normal.jpg", "resources/floor_specular.jpg");
-            this.planeMaterial["specular"] = 10;
-            this.particleMaterial = new StandardMaterial("resources/blue.png");
-            this.particleMaterial.diffuseMethod.difuseTexture.format = feng3d.GL.RGBA;
-            this.particleMaterial.enableBlend = true;
-        }
+    //     private initMaterials()
+    //     {
+    //         this.planeMaterial = new StandardMaterial("resources/floor_diffuse.jpg", "resources/floor_normal.jpg", "resources/floor_specular.jpg");
+    //         this.planeMaterial["specular"] = 10;
+    //         this.particleMaterial = new StandardMaterial("resources/blue.png");
+    //         this.particleMaterial.diffuseMethod.difuseTexture.format = feng3d.GL.RGBA;
+    //         this.particleMaterial.enableBlend = true;
+    //     }
 
-        private initParticles()
-        {
-            this.fireAnimationSet = new ParticleAnimator();
-            this.fireAnimationSet.addComponent(new ParticleBillboard());
-            this.fireAnimationSet["addAnimation"](new ParticleScaleNode(ParticlePropertiesMode.GLOBAL, false, false, 2.5, 0.5));
-            this.fireAnimationSet["addAnimation"](new ParticleVelocityNode(ParticlePropertiesMode.GLOBAL, new Vector3D(0, 80, 0)));
-            this.fireAnimationSet["addAnimation"](new ParticleColorNode(ParticlePropertiesMode.GLOBAL, true, true, false, false, new flash.ColorTransform(0, 0, 0, 1, 0xFF, 0x33, 0x01), new flash.ColorTransform(0, 0, 0, 1, 0x99)));
-            this.fireAnimationSet["addAnimation"](new ParticleVelocityNode(ParticlePropertiesMode.LOCAL_STATIC));
-            //通过函数来创建粒子初始状态
-            this.fireAnimationSet.numParticles = 500;
-            this.fireAnimationSet.generateFunctions.push({
-                generate: (particle) =>
-                {
-                    particle.color = new Color(1, 0, 0, 1).mix(new Color(0, 1, 0, 1), particle.index / particle.total);
+    //     private initParticles()
+    //     {
+    //         this.fireAnimationSet = new ParticleAnimator();
+    //         this.fireAnimationSet.addComponent(new ParticleBillboard());
+    //         this.fireAnimationSet["addAnimation"](new ParticleScaleNode(ParticlePropertiesMode.GLOBAL, false, false, 2.5, 0.5));
+    //         this.fireAnimationSet["addAnimation"](new ParticleVelocityNode(ParticlePropertiesMode.GLOBAL, new Vector3D(0, 80, 0)));
+    //         this.fireAnimationSet["addAnimation"](new ParticleColorNode(ParticlePropertiesMode.GLOBAL, true, true, false, false, new flash.ColorTransform(0, 0, 0, 1, 0xFF, 0x33, 0x01), new flash.ColorTransform(0, 0, 0, 1, 0x99)));
+    //         this.fireAnimationSet["addAnimation"](new ParticleVelocityNode(ParticlePropertiesMode.LOCAL_STATIC));
+    //         //通过函数来创建粒子初始状态
+    //         this.fireAnimationSet.numParticles = 500;
+    //         this.fireAnimationSet.generateFunctions.push({
+    //             generate: (particle) =>
+    //             {
+    //                 particle.color = new Color(1, 0, 0, 1).mix(new Color(0, 1, 0, 1), particle.index / particle.total);
 
-                    particle.birthTime = Math.random() * 5;
-                    particle.lifetime = Math.random() * 4 + 0.1;
-                    var degree1: number = Math.random() * Math.PI * 2;
-                    var degree2: number = Math.random() * Math.PI * 2;
-                    var r: number = <any>15;
-                    particle.velocity = new Vector3D(r * Math.sin(degree1) * Math.cos(degree2), r * Math.cos(degree1) * Math.cos(degree2), r * Math.sin(degree2));
-                }, priority: 0
-            });
-            this.particleGeometry = new PlaneGeometry(10, 10, 1, 1, false);
-        }
+    //                 particle.birthTime = Math.random() * 5;
+    //                 particle.lifetime = Math.random() * 4 + 0.1;
+    //                 var degree1: number = Math.random() * Math.PI * 2;
+    //                 var degree2: number = Math.random() * Math.PI * 2;
+    //                 var r: number = <any>15;
+    //                 particle.velocity = new Vector3D(r * Math.sin(degree1) * Math.cos(degree2), r * Math.cos(degree1) * Math.cos(degree2), r * Math.sin(degree2));
+    //             }, priority: 0
+    //         });
+    //         this.particleGeometry = new PlaneGeometry(10, 10, 1, 1, false);
+    //     }
 
-        private initObjects()
-        {
-            this.plane = new Mesh(new PlaneGeometry(1000, 1000), this.planeMaterial);
-            this.plane["geometry"].scaleUV(2, 2);
-            this.plane["y"] = -20;
-            this.scene["addChild"](this.plane);
-            for (var i: number = 0; i < Basic_Fire.NUM_FIRES; i++)
-            {
-                var particleMesh = new GameObject();
-                var model = particleMesh.getOrCreateComponentByClass(Model);
-                model.geometry = this.particleGeometry;
-                model.material = this.particleMaterial;
-                particleMesh.addComponent(this.fireAnimationSet);
-                var degree: number = i / Basic_Fire.NUM_FIRES * Math.PI * 2;
-                particleMesh.x = Math.sin(degree) * 400;
-                particleMesh.z = Math.cos(degree) * 400;
-                particleMesh.y = 5;
-                this.fireObjects.push(new FireVO(particleMesh));
-                this.view["scene"].addChild(particleMesh);
-            }
-            this.timer = new Timer(1000, this.fireObjects.length);
-            this.timer.addEventListener(TimerEvent.TIMER, this.onTimer, this);
-            this.timer.start();
-        }
+    //     private initObjects()
+    //     {
+    //         this.plane = new Mesh(new PlaneGeometry(1000, 1000), this.planeMaterial);
+    //         this.plane["geometry"].scaleUV(2, 2);
+    //         this.plane["y"] = -20;
+    //         this.scene["addChild"](this.plane);
+    //         for (var i: number = 0; i < Basic_Fire.NUM_FIRES; i++)
+    //         {
+    //             var particleMesh = new GameObject();
+    //             var model = particleMesh.getOrCreateComponentByClass(Model);
+    //             model.geometry = this.particleGeometry;
+    //             model.material = this.particleMaterial;
+    //             particleMesh.addComponent(this.fireAnimationSet);
+    //             var degree: number = i / Basic_Fire.NUM_FIRES * Math.PI * 2;
+    //             particleMesh.x = Math.sin(degree) * 400;
+    //             particleMesh.z = Math.cos(degree) * 400;
+    //             particleMesh.y = 5;
+    //             this.fireObjects.push(new FireVO(particleMesh));
+    //             this.view["scene"].addChild(particleMesh);
+    //         }
+    //         this.timer = new Timer(1000, this.fireObjects.length);
+    //         this.timer.addEventListener(TimerEvent.TIMER, this.onTimer, this);
+    //         this.timer.start();
+    //     }
 
-        private initListeners()
-        {
-            ticker.addEventListener(Event.ENTER_FRAME, this.onEnterFrame, this);
-            input.addEventListener(inputType.MOUSE_DOWN, this.onMouseDown, this);
-            input.addEventListener(inputType.MOUSE_UP, this.onMouseUp, this);
-        }
+    //     private initListeners()
+    //     {
+    //         ticker.addEventListener(Event.ENTER_FRAME, this.onEnterFrame, this);
+    //         input.addEventListener(inputType.MOUSE_DOWN, this.onMouseDown, this);
+    //         input.addEventListener(inputType.MOUSE_UP, this.onMouseUp, this);
+    //     }
 
-        private getAllLights(): Array<any>
-        {
-            var lights: Array<any> = new Array();
-            lights.push(this.directionalLight);
-            for (var fireVO_key_a in this.fireObjects)
-            {
-                var fireVO: FireVO = this.fireObjects[fireVO_key_a];
-                if (fireVO.light)
-                    lights.push(fireVO.light);
-            }
-            return lights;
-        }
+    //     private getAllLights(): Array<any>
+    //     {
+    //         var lights: Array<any> = new Array();
+    //         lights.push(this.directionalLight);
+    //         for (var fireVO_key_a in this.fireObjects)
+    //         {
+    //             var fireVO: FireVO = this.fireObjects[fireVO_key_a];
+    //             if (fireVO.light)
+    //                 lights.push(fireVO.light);
+    //         }
+    //         return lights;
+    //     }
 
-        private onTimer(e: TimerEvent)
-        {
-            var fireObject: FireVO = this.fireObjects[this.timer.currentCount - 1];
-            fireObject.animator["start"]();
-            var light: PointLight = <any>new PointLight();
-            light.color.fromUnit(0xFF3301);
-            light["diffuse"] = 0;
-            light["specular"] = 0;
-            light["position"] = fireObject.mesh["position"];
-            fireObject.light = light;
-            this.lightPicker["lights"] = this.getAllLights();
-        }
+    //     private onTimer(e: TimerEvent)
+    //     {
+    //         var fireObject: FireVO = this.fireObjects[this.timer.currentCount - 1];
+    //         fireObject.animator["start"]();
+    //         var light: PointLight = <any>new PointLight();
+    //         light.color.fromUnit(0xFF3301);
+    //         light["diffuse"] = 0;
+    //         light["specular"] = 0;
+    //         light["position"] = fireObject.mesh["position"];
+    //         fireObject.light = light;
+    //         this.lightPicker["lights"] = this.getAllLights();
+    //     }
 
-        private onEnterFrame(event: Event)
-        {
-            if (this.move)
-            {
-                this.cameraController.panAngle = 0.3 * (this.view.mousePos.x - this.lastMouseX) + this.lastPanAngle;
-                this.cameraController.tiltAngle = 0.3 * (this.view.mousePos.y - this.lastMouseY) + this.lastTiltAngle;
-            }
-            var fireVO: FireVO;
-            var fireVO_key_a;
-            for (fireVO_key_a in this.fireObjects)
-            {
-                fireVO = this.fireObjects[fireVO_key_a];
-                var light: PointLight = fireVO.light;
-                if (<any>!light)
-                    continue;
-                if (fireVO.strength < 1)
-                    fireVO.strength += 0.1;
-                light["fallOff"] = 380 + Math.random() * 20;
-                light["radius"] = 200 + Math.random() * 30;
-                light["diffuse"] = light["specular"] = fireVO.strength + Math.random() * .2;
-            }
-            this.view["render"]();
-        }
+    //     private onEnterFrame(event: Event)
+    //     {
+    //         if (this.move)
+    //         {
+    //             this.cameraController.panAngle = 0.3 * (this.view.mousePos.x - this.lastMouseX) + this.lastPanAngle;
+    //             this.cameraController.tiltAngle = 0.3 * (this.view.mousePos.y - this.lastMouseY) + this.lastTiltAngle;
+    //         }
+    //         var fireVO: FireVO;
+    //         var fireVO_key_a;
+    //         for (fireVO_key_a in this.fireObjects)
+    //         {
+    //             fireVO = this.fireObjects[fireVO_key_a];
+    //             var light: PointLight = fireVO.light;
+    //             if (<any>!light)
+    //                 continue;
+    //             if (fireVO.strength < 1)
+    //                 fireVO.strength += 0.1;
+    //             light["fallOff"] = 380 + Math.random() * 20;
+    //             light["radius"] = 200 + Math.random() * 30;
+    //             light["diffuse"] = light["specular"] = fireVO.strength + Math.random() * .2;
+    //         }
+    //         this.view["render"]();
+    //     }
 
-        private onMouseDown(event: Event)
-        {
-            this.lastPanAngle = this.cameraController.panAngle;
-            this.lastTiltAngle = this.cameraController.tiltAngle;
-            this.lastMouseX = this.view.mousePos.x;
-            this.lastMouseY = this.view.mousePos.y;
-            this.move = true;
-        }
+    //     private onMouseDown(event: Event)
+    //     {
+    //         this.lastPanAngle = this.cameraController.panAngle;
+    //         this.lastTiltAngle = this.cameraController.tiltAngle;
+    //         this.lastMouseX = this.view.mousePos.x;
+    //         this.lastMouseY = this.view.mousePos.y;
+    //         this.move = true;
+    //     }
 
-        private onMouseUp(event: Event)
-        {
-            this.move = false;
-        }
-    }
+    //     private onMouseUp(event: Event)
+    //     {
+    //         this.move = false;
+    //     }
+    // }
 
-    class FireVO
-    {
-        public mesh: GameObject;
-        public animator: ParticleAnimator;
-        public light: PointLight;
-        public strength: number = 0;
+    // class FireVO
+    // {
+    //     public mesh: GameObject;
+    //     public animator: ParticleAnimator;
+    //     public light: PointLight;
+    //     public strength: number = 0;
 
-        public constructor(mesh: GameObject, animator: ParticleAnimator = null)
-        {
-            this.mesh = mesh;
-            this.animator = animator;
-        }
+    //     public constructor(mesh: GameObject, animator: ParticleAnimator = null)
+    //     {
+    //         this.mesh = mesh;
+    //         this.animator = animator;
+    //     }
 
-    }
+    // }
 }

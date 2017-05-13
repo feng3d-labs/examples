834d7c83659a19537a94b5b8f8a096ed1391e1a5
feng <908087098@qq.com>
2017-05-13 16:27:29 +0800

    错误的合并地表线性混合shader

--- feng3d/shaders/modules/terrainMerge.fragment.glsl
+++ feng3d/shaders/modules/terrainMerge.fragment.glsl
@@ -9,12 +9,10 @@ uniform vec4 u_splatRepeats;
 
 vec2 imageSize =    vec2(2048.0,1024.0);
 vec4 offset[3];
-vec2 tileSize = vec2(512.0,512.0);
+vec2 tileSize = vec2(1024.0,1024.0);
 float maxLod = 7.0;
 
 vec4 terrainTexture2DLod(sampler2D s_splatMergeTexture,vec2 t_uv,float lod,vec4 offset){
-
-        // t_uv = t_uv * offset[i].xy + offset[i].zw;
 
     //计算不同lod像素缩放以及起始坐标
     vec4 lodvec = vec4(0.5,1.0,0.0,0.0);
@@ -22,17 +20,47 @@ vec4 terrainTexture2DLod(sampler2D s_splatMergeTexture,vec2 t_uv,float lod,vec4 
     lodvec.y = lodvec.x * 2.0;
     lodvec.z = 1.0 - lodvec.y;
 
-    //lod块尺寸
-    vec2 lodSize = lodvec.xy * imageSize;
-    vec4 lodOffset = lodSize.xyxy * offset;
-    //lod块中像素索引
-    t_uv = t_uv * lodSize;
+    vec2 lodPixelOffset = vec2(1.0,1.0) * lodvec.xy;
+    vec2 mixFactor = mod(t_uv, lodPixelOffset);
+
+    t_uv = fract(t_uv + vec2(0.0,0.0) * lodPixelOffset);
+    //
+    t_uv = (t_uv * offset.xy + offset.zw) * lodvec.xy;
     //取整像素
-    t_uv = (mod(floor(t_uv + vec2(1.0,0.0)),lodOffset.xy) + lodOffset.zw) / imageSize;
+    t_uv = floor(t_uv * imageSize) / imageSize;
     //添加lod起始坐标
     t_uv = t_uv + lodvec.zw;
-    
-    vec4 tColor = texture2D(s_splatMergeTexture,t_uv);
+    vec4 tColor00 = texture2D(s_splatMergeTexture,t_uv);
+
+    t_uv = fract(t_uv + vec2(1.0,0.0) * lodPixelOffset);
+    //
+    t_uv = (t_uv * offset.xy + offset.zw) * lodvec.xy;
+    //取整像素
+    t_uv = floor(t_uv * imageSize) / imageSize;
+    //添加lod起始坐标
+    t_uv = t_uv + lodvec.zw;
+    vec4 tColor10 = texture2D(s_splatMergeTexture,t_uv);
+
+    t_uv = fract(t_uv + vec2(0.0,1.0) * lodPixelOffset);
+    //
+    t_uv = (t_uv * offset.xy + offset.zw) * lodvec.xy;
+    //取整像素
+    t_uv = floor(t_uv * imageSize) / imageSize;
+    //添加lod起始坐标
+    t_uv = t_uv + lodvec.zw;
+    vec4 tColor01 = texture2D(s_splatMergeTexture,t_uv);
+
+    t_uv = fract(t_uv + vec2(1.0,1.0) * lodPixelOffset);
+    //
+    t_uv = (t_uv * offset.xy + offset.zw) * lodvec.xy;
+    //取整像素
+    t_uv = floor(t_uv * imageSize) / imageSize;
+    //添加lod起始坐标
+    t_uv = t_uv + lodvec.zw;
+    vec4 tColor11 = texture2D(s_splatMergeTexture,t_uv);
+
+    vec4 tColor = tColor00 * (1.0 - mixFactor.x) * (1.0 - mixFactor.y) + tColor10 * mixFactor.x * (1.0 - mixFactor.y) + tColor01 * (1.0 - mixFactor.x) * mixFactor.y + tColor11 * mixFactor.x * mixFactor.y;
+
     return tColor;
 }
 
@@ -40,8 +68,8 @@ vec4 terrainTexture2DLod(sampler2D s_splatMergeTexture,vec2 t_uv,float lod,vec4 
 //计算MipMap层函数：
 float mipmapLevel(vec2 uv, vec2 textureSize)
 {
-    vec2 dx = dFdx(uv * textureSize.x);
-    vec2 dy = dFdy(uv * textureSize.y);
+    float dx = dFdx(uv.x * textureSize.x);
+    float dy = dFdy(uv.y * textureSize.y);
     float d = max(dot(dx, dx), dot(dy, dy));  
     return 0.5 * log2(d);
 }
@@ -69,7 +97,6 @@ vec4 terrainMethod(vec4 diffuseColor,vec2 v_uv) {
         vec2 t_uv = v_uv.xy * u_splatRepeats[i];
         float lod = mipmapLevel(t_uv,tileSize);
         lod = clamp(lod,0.0,maxLod);
-        t_uv = fract(t_uv);
         vec4 tColor = terrainTexture2D(s_splatMergeTexture,t_uv,lod,offset[i]);
         diffuseColor = (tColor - diffuseColor) * blend[i] + diffuseColor;
     }

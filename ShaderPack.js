613af70d07b24101d83caca92b8adbcb6558388a
feng <908087098@qq.com>
2017-06-05 11:29:37 +0800

    调整 ShaderPack

--- ShaderPack.js
+++ ShaderPack.js
@@ -1,17 +1,18 @@
 var fs = require("fs");
 const util = require("util");
-const debuglog = util.debuglog('foo');
+// const debuglog = util.debuglog('foo');
+const debuglog = console.log;
 
 var root = "feng3d/";
 
-// fs.watch("./shaders", (event, filename) => {
-//     try 
-var savePath = root + "src/autofiles/shaders.ts";
-var filesContent = readFiles(getFilePaths(root + "shaders"));
-var contentStr = JSON.stringify(filesContent, null, '\t').replace(/[\n\t]+([\d\.e\-\[\]]+)/g, '$1');
-contentStr = contentStr.replace(new RegExp(root, "g"), "");
-writeFile(savePath, `module feng3d\n{\nfeng3d.shaderFileMap = ${contentStr}\n}`);
-debuglog("自动生成" + savePath)
+// fs.watch(root + "shaders", (event, filename) => {
+//     try {
+        var savePath = root + "src/autofiles/shaders.ts";
+        var filesContent = readFiles(getFilePaths(root + "shaders"));
+        var contentStr = JSON.stringify(filesContent, null, '\t').replace(/[\n\t]+([\d\.e\-\[\]]+)/g, '$1');
+        contentStr = contentStr.replace(new RegExp(root, "g"), "");
+        writeFile(savePath, `module feng3d\n{\nfeng3d.shaderFileMap = ${contentStr}\n}`);
+        debuglog("自动生成" + savePath)
 //     } catch (error) {
 //         debuglog("error!!!!!\n" + error);
 //     }

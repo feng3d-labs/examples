3e7a64bd490273b5f049f631d185077b7a4adcc1
feng <908087098@qq.com>
2017-06-24 11:53:41 +0800

    配置 feng3d-examples环境

---
+++ ShaderPack.js
@@ -0,0 +1,53 @@
+var fs = require("fs");
+const util = require("util");
+// const debuglog = util.debuglog('foo');
+const debuglog = console.log;
+
+var root = "feng3d/";
+
+// fs.watch(root + "shaders", (event, filename) => {
+//     try {
+        var savePath = root + "src/autofiles/shaders.ts";
+        var filesContent = readFiles(getFilePaths(root + "shaders"));
+        var contentStr = JSON.stringify(filesContent, null, '\t').replace(/[\n\t]+([\d\.e\-\[\]]+)/g, '$1');
+        contentStr = contentStr.replace(new RegExp(root, "g"), "");
+        writeFile(savePath, `namespace feng3d\n{\nfeng3d.shaderFileMap = ${contentStr}\n}`);
+        debuglog("自动生成" + savePath)
+//     } catch (error) {
+//         debuglog("error!!!!!\n" + error);
+//     }
+// });
+
+function writeFile(filePath, content) {
+    fs.openSync(filePath, "w");
+    fs.writeFileSync(filePath, content);
+}
+
+function readFile(filePath) {
+    fs.openSync(filePath, "r");
+    var result = fs.readFileSync(filePath, 'utf8');
+    return result;
+}
+
+function readFiles(filePaths) {
+    var result = {};
+    filePaths.forEach(function (element) {
+        result[element] = readFile(element);
+    }, this);
+    return result;
+}
+
+function getFilePaths(rootPath, filePaths) {
+
+    filePaths = filePaths || [];
+    var stats = fs.statSync(rootPath);
+    if (stats.isFile()) {
+        filePaths.push(rootPath);
+    } else if (stats.isDirectory) {
+        var childPaths = fs.readdirSync(rootPath);
+        for (var i = 0; i < childPaths.length; i++) {
+            getFilePaths(rootPath + "/" + childPaths[i], filePaths);
+        }
+    }
+    return filePaths;
+}

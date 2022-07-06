408cf13a0930cda594db1cce0a47055d457b6def
feng <908087098@qq.com>
2022-07-06 14:33:17 +0800

    更新 字体 示例

--- index.js
+++ index.js
@@ -39,11 +39,16 @@ var files = {
         "Basic_SkyBox",
         "Basic_Shading",
     ],
+    "font": [
+        "GeometryFontTest"
+    ]
 };
 
-function extractQuery() {
+function extractQuery()
+{
     var p = window.location.search.indexOf('?q=');
-    if (p !== -1) {
+    if (p !== -1)
+    {
         return window.location.search.substr(3);
     }
     return ''
@@ -57,14 +62,16 @@ var filterInput = document.getElementById('filterInput');
 var clearFilterButton = document.getElementById('clearFilterButton');
 
 var expandButton = document.getElementById('expandButton');
-expandButton.addEventListener('click', function (event) {
+expandButton.addEventListener('click', function (event)
+{
     panel.classList.toggle('collapsed');
     event.preventDefault();
 });
 
 // iOS iframe auto-resize workaround
 
-if (/(iPad|iPhone|iPod)/g.test(navigator.userAgent)) {
+if (/(iPad|iPhone|iPod)/g.test(navigator.userAgent))
+{
 
     viewer.style.width = getComputedStyle(viewer).width;
     viewer.style.height = getComputedStyle(viewer).height;
@@ -78,7 +85,8 @@ content.appendChild(container);
 var button = document.createElement('div');
 button.id = 'button';
 button.textContent = 'View source';
-button.addEventListener('click', function (event) {
+button.addEventListener('click', function (event)
+{
 
     window.open('https://gitee.com/feng3d/examples/tree/master/src/' + selected + '.ts');
 
@@ -90,7 +98,8 @@ var links = {};
 var paths = {};
 var selected = null;
 
-for (var key in files) {
+for (var key in files)
+{
 
     var section = files[key];
 
@@ -99,20 +108,24 @@ for (var key in files) {
     header.setAttribute('data-category', key);
     container.appendChild(header);
 
-    for (var i = 0; i < section.length; i++) {
+    for (var i = 0; i < section.length; i++)
+    {
 
-        (function (file) {
+        (function (file)
+        {
 
             paths[file] = key + "/" + file;
 
             var link = document.createElement('a');
             link.className = 'link';
             link.textContent = file;
-            link.href = "examples.html?type=" + paths[file] + "&v="+Math.random();
+            link.href = "examples.html?type=" + paths[file] + "&v=" + Math.random();
             link.setAttribute('target', 'viewer');
-            link.addEventListener('click', function (event) {
+            link.addEventListener('click', function (event)
+            {
 
-                if (event.button === 0) {
+                if (event.button === 0)
+                {
 
                     selectFile(file);
 
@@ -129,14 +142,16 @@ for (var key in files) {
 
 }
 
-function loadFile(file) {
+function loadFile(file)
+{
 
     selectFile(file);
     viewer.src = "examples.html?type=" + paths[file];
 
 }
 
-function selectFile(file) {
+function selectFile(file)
+{
 
     if (selected !== null) links[selected].classList.remove('selected');
 
@@ -152,7 +167,8 @@ function selectFile(file) {
 
 }
 
-if (window.location.hash !== '') {
+if (window.location.hash !== '')
+{
 
     loadFile(window.location.hash.substring(1));
 
@@ -160,13 +176,15 @@ if (window.location.hash !== '') {
 
 // filter
 
-filterInput.addEventListener('input', function (e) {
+filterInput.addEventListener('input', function (e)
+{
 
     updateFilter();
 
 });
 
-clearFilterButton.addEventListener('click', function (e) {
+clearFilterButton.addEventListener('click', function (e)
+{
 
     filterInput.value = '';
     updateFilter();
@@ -174,22 +192,27 @@ clearFilterButton.addEventListener('click', function (e) {
 
 });
 
-function updateFilter() {
+function updateFilter()
+{
 
     var v = filterInput.value;
-    if (v !== '') {
+    if (v !== '')
+    {
         window.history.replaceState({}, '', '?q=' + v + window.location.hash);
-    } else {
+    } else
+    {
         window.history.replaceState({}, '', window.location.pathname + window.location.hash);
     }
 
     var exp = new RegExp(v, 'gi');
 
-    for (var key in files) {
+    for (var key in files)
+    {
 
         var section = files[key];
 
-        for (var i = 0; i < section.length; i++) {
+        for (var i = 0; i < section.length; i++)
+        {
 
             filterExample(section[i], exp);
 
@@ -201,23 +224,27 @@ function updateFilter() {
 
 }
 
-function filterExample(file, exp) {
+function filterExample(file, exp)
+{
 
     var link = links[file];
     var res = file.match(exp);
     var text;
 
-    if (res && res.length > 0) {
+    if (res && res.length > 0)
+    {
 
         link.classList.remove('filtered');
 
-        for (var i = 0; i < res.length; i++) {
+        for (var i = 0; i < res.length; i++)
+        {
             text = file.replace(res[i], '<b>' + res[i] + '</b>');
         }
 
         link.innerHTML = text;
 
-    } else {
+    } else
+    {
 
         link.classList.add('filtered');
         link.innerHTML = file;
@@ -225,19 +252,23 @@ function filterExample(file, exp) {
     }
 }
 
-function layoutList() {
+function layoutList()
+{
 
-    for (var key in files) {
+    for (var key in files)
+    {
 
         var collapsed = true;
 
         var section = files[key];
 
-        for (var i = 0; i < section.length; i++) {
+        for (var i = 0; i < section.length; i++)
+        {
 
             var file = section[i];
 
-            if (!links[file].classList.contains('filtered')) {
+            if (!links[file].classList.contains('filtered'))
+            {
 
                 collapsed = false;
                 break;
@@ -248,11 +279,13 @@ function layoutList() {
 
         var element = document.querySelector('h2[data-category="' + key + '"]');
 
-        if (collapsed) {
+        if (collapsed)
+        {
 
             element.classList.add('filtered');
 
-        } else {
+        } else
+        {
 
             element.classList.remove('filtered');
 

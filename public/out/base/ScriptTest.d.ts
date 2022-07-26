5398696c34019b8619c62ef8876b60391bcccfb9
feng <908087098@qq.com>
2022-07-26 20:22:08 +0800

    deploy

---
+++ public/out/base/ScriptTest.d.ts
@@ -0,0 +1,11 @@
+declare namespace examples {
+}
+declare class ScriptDemo extends feng3d.Script {
+    cube: feng3d.GameObject;
+    init(): void;
+    update(): void;
+    /**
+     * 销毁
+     */
+    dispose(): void;
+}

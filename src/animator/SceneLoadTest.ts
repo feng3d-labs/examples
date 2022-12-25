c275e343216d6d039b8b3377e8c21bb0aa662887
feng <908087098@qq.com>
2022-12-26 07:23:13 +0800

    serialization.deserialize -> $deserialize

--- src/animator/SceneLoadTest.ts
+++ src/animator/SceneLoadTest.ts
@@ -1,11 +1,11 @@
-import { loader, Node3D, Scene, $set, View } from 'feng3d';
+import { loader, Node3D, Scene, $set, View, $deserialize } from 'feng3d';
 
 const view3D = new View();
 
 loader.loadText('../../../scene/Untitled.scene.json').then((content) =>
 {
     const json = JSON.parse(content);
-    const sceneobject: Node3D = serialization.deserialize(json);
+    const sceneobject: Node3D = $deserialize(json);
     const scene = sceneobject.getComponent(Scene);
 
     view3D.scene = scene;

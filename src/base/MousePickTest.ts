2e7971522706c107cbfcd1c6d6077265b6a4d2aa
feng <908087098@qq.com>
2023-01-04 21:49:19 +0800

    调整示例

--- src/base/MousePickTest.ts
+++ src/base/MousePickTest.ts
@@ -1,4 +1,4 @@
-import { Camera, Color4, FPSController, MeshRenderer, Node3D, Scene, $set, StandardMaterial, StandardUniforms, Vector3, View3D } from 'feng3d';
+import { Camera, Color4, FPSController, MeshRenderer, Node3D, Scene, $set, StandardMaterial, StandardUniforms, Vector3, View3D, ColorMaterial } from 'feng3d';
 
 /**
  * 操作方式:鼠标按下后可以使用移动鼠标改变旋转，wasdqe平移
@@ -18,25 +18,25 @@ camera.node3d.addComponent(FPSController);
 
 const cube = Node3D.createPrimitive('Cube');
 cube.mouseEnabled = true;
-cube.getComponent(MeshRenderer).material = new StandardMaterial();
+cube.getComponent(MeshRenderer).material = new ColorMaterial();
 scene.node3d.addChild(cube);
 
 const sphere = Node3D.createPrimitive('Sphere');
 sphere.position = new Vector3(-1.50, 0, 0);
 sphere.mouseEnabled = true;
-sphere.getComponent(MeshRenderer).material = new StandardMaterial();
+sphere.getComponent(MeshRenderer).material = new ColorMaterial();
 scene.node3d.addChild(sphere);
 
 const capsule = Node3D.createPrimitive('Capsule');
 capsule.position = new Vector3(3, 0, 0);
 capsule.mouseEnabled = true;
-capsule.getComponent(MeshRenderer).material = new StandardMaterial();
+capsule.getComponent(MeshRenderer).material = new ColorMaterial();
 scene.node3d.addChild(capsule);
 
 const cylinder = Node3D.createPrimitive('Cylinder');
 cylinder.position = new Vector3(-3, 0, 0);
 cylinder.mouseEnabled = true;
-cylinder.getComponent(MeshRenderer).material = new StandardMaterial();
+cylinder.getComponent(MeshRenderer).material = new ColorMaterial();
 scene.node3d.addChild(cylinder);
 
 scene.on('click', (event) =>
@@ -44,7 +44,7 @@ scene.on('click', (event) =>
     const node3d = <Node3D>event.target;
     if (node3d.getComponent(MeshRenderer))
     {
-        const uniforms = <StandardUniforms>node3d.getComponent(MeshRenderer).material.uniforms;
-        uniforms.u_diffuse.fromUnit(Math.random() * (1 << 24));
+        const material = node3d.getComponent(MeshRenderer).material as ColorMaterial;
+        material.uniforms.u_diffuseInput.random();
     }
 });

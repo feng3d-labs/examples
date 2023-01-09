a1fb2ec43bd271a4426736f45563b9aad6436f03
feng <908087098@qq.com>
2023-01-09 09:22:14 +0800

    Geometry.create

--- src/base/FPSControllerTest.ts
+++ src/base/FPSControllerTest.ts
@@ -1,4 +1,4 @@
-import { CapsuleGeometry, Color4, ColorMaterial, CubeGeometry, CylinderGeometry, Node3D, PlaneGeometry, SphereGeometry, Vector3 } from 'feng3d';
+import { Color4, ColorMaterial, Geometry, Node3D, Vector3 } from 'feng3d';
 
 const root = new Node3D();
 root.addComponent('View3D');
@@ -12,34 +12,34 @@ root.addChild(camera.node3d);
 
 const cube = new Node3D().addComponent('MeshRenderer', {
     material: new ColorMaterial(),
-    geometry: new CubeGeometry(),
+    geometry: Geometry.create('CubeGeometry'),
 }).node3d;
 scene.node3d.addChild(cube);
 
 const sphere = new Node3D().addComponent('MeshRenderer', {
     material: new ColorMaterial(),
-    geometry: new SphereGeometry(),
+    geometry: Geometry.create('SphereGeometry'),
 }).node3d;
 sphere.position = new Vector3(-1.50, 0, 0);
 scene.node3d.addChild(sphere);
 
 const plane = new Node3D().addComponent('MeshRenderer', {
     material: new ColorMaterial(),
-    geometry: new PlaneGeometry(),
+    geometry: Geometry.create('PlaneGeometry'),
 }).node3d;
 plane.position = new Vector3(1.50, 0, 0);
 scene.node3d.addChild(plane);
 
 const capsule = new Node3D().addComponent('MeshRenderer', {
     material: new ColorMaterial(),
-    geometry: new CapsuleGeometry(),
+    geometry: Geometry.create('CapsuleGeometry'),
 }).node3d;
 capsule.position = new Vector3(3, 0, 0);
 scene.node3d.addChild(capsule);
 
 const cylinder = new Node3D().addComponent('MeshRenderer', {
     material: new ColorMaterial(),
-    geometry: new CylinderGeometry(),
+    geometry: Geometry.create('CylinderGeometry'),
 }).node3d;
 cylinder.position = new Vector3(-3, 0, 0);
 scene.node3d.addChild(cylinder);
